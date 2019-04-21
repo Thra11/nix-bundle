69332ecaa83dbf37b57fd631468aa23ff0d54812
Tom Hall <tahall256@protonmail.ch>
2019-04-21 12:41:39 +0100

    Move nix-user-chroot into nixpkgs. Remove old definition to avoid confusion

--- default.nix
+++ default.nix
@@ -34,32 +34,6 @@ in rec {
       '';
     };
 
-  # TODO: eventually should this go in nixpkgs?
-  nix-user-chroot = stdenv.lib.makeOverridable stdenv.mkDerivation {
-    name = "nix-user-chroot-2c52b5f";
-    src = ./nix-user-chroot;
-
-    makeFlags = [];
-
-    # hack to use when /nix/store is not available
-    postFixup = ''
-      exe=$out/bin/nix-user-chroot
-      patchelf \
-        --set-interpreter .$(patchelf --print-interpreter $exe) \
-        --set-rpath $(patchelf --print-rpath $exe | sed 's|/nix/store/|./nix/store/|g') \
-        $exe
-    '';
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/bin/
-      cp nix-user-chroot $out/bin/nix-user-chroot
-
-      runHook postInstall
-    '';
-  };
-
   makebootstrap = { targets, startup }:
     arx {
       inherit startup;
